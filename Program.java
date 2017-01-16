2ff71b0d2efeeb28ee27fb2d45a1d1cda9152e5d
Hidrai <grazvydas1997@gmail.com>
2017-01-16 10:56:30 +0200

    Input

--- Program.java
+++ Program.java
@@ -2,13 +2,20 @@ import java.util.Scanner;
 
 public class Program
 {
-    public static void main (String[] args)
-    {
-        System.out.println ("Hello");
-    }
+	Scanner sc = new Scanner (System.in);
 
-    public int sum (int a, int b)
-    {
-	return a+b;
-    }
+    	public static void main (String[] args)
+    	{
+        	System.out.println ("Hello");
+    	}
+
+	public int input (String text)
+	{
+		System.out.print(text);
+		return sc.nextInt();
+	}
+    	public int sum (int a, int b)
+    	{
+		return a+b;
+    	}
 }
