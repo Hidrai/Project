3cf3f5bc0a0129d7ed78c16d1ab6df3f77871522
Hidrai <grazvydas1997@gmail.com>
2017-01-16 11:23:40 +0200

    Multi

--- Program.java
+++ Program.java
@@ -1,12 +1,17 @@
 public class Program
 {
-    public static void main (String[] args)
-    {
-        System.out.println ("Hello");
-    }
+    	public static void main (String[] args)
+    	{
+        	System.out.println ("Hello");
+    	}
 
-    public int sum (int a, int b)
-    {
-	return a+b;
-    }
+    	public int sum (int a, int b)
+    	{
+		return a+b;
+    	}
+	
+	public int mul (int a, int b)
+	{
+		return a*b;	
+	}
 }
